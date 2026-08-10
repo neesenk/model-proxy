cd1890f845e3a9383c8f778adca0d637357e246a
Zhiyong Liu <neesenk@gmail.com>
2026-08-10 21:51:59 +0800

    refactor(cli): drop unused root cli_run helpers

--- model-proxy/cli_run.go
+++ model-proxy/cli_run.go
@@ -9,10 +9,6 @@ import (
 // cliCommand aliases the internal/cli command contract.
 type cliCommand = clicmd.Command
 
-func processCLICommand(run func([]string)) cliCommand {
-	return clicmd.ProcessCommand(run)
-}
-
 func runCLIArgsWithCommands(
 	args []string,
 	stdin io.Reader,
@@ -21,5 +17,3 @@ func runCLIArgsWithCommands(
 ) int {
 	return clicmd.RunArgsWithCommands(args, stdin, stdout, stderr, commands)
 }
-
-func hasHelpFlag(args []string) bool { return clicmd.HasHelpFlag(args) }
